a2822c75b03620141cb6cc2326a4a677ae667be4
Mikhail Porsev <mporsev@mail.ru>
2025-01-17 20:23:20 +0400

    Затенил bg добавил новую страничку и другое (#8)

--- develop/cringe/main/views.py
+++ develop/cringe/main/views.py
@@ -4,8 +4,13 @@ from django.shortcuts import render
 def index(request):
     context: dict = {
         'title': 'Front-End',
-
+        'content': "Front-End разработка"
     }
     return render(request, 'main/index.html', context)
 def general(request):
-    return HttpResponse('General Page')
+    context: dict = {
+        'title': 'Общее',
+        'content': "Общая информация",
+        'text_on_page': "Какой то текст"
+    }
+    return render(request, 'main/general.html', context)
